cd8c5c9bef517c223a0d8a165b1e82d7e6403836
Felipe Mateus <felipe_mateus08@hotmail.com>
2023-01-18 17:21:50 -0300

    docs: first configuration

--- App.tsx
+++ App.tsx
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Loading } from './src/components/Loading';
+
+import { 
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  } from '@expo-google-fonts/inter';
+
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <Loading />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.Text}>Haruo lindao</Text>
+      <StatusBar style='light' />
     </View>
   );
 }
@@ -13,8 +36,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#09090A',
+    color: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Text: {
+    color: '#fff',
+    fontSize: 30,
+    fontFamily: 'Inter_700Bold',
+  }
 });
